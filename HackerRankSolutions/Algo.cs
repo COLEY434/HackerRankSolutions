327334400e1725a510320226fdd597799f1b3e19
Collins Anieze <CAnieze_Dev@polarisbanklimited.com>
2021-06-29 11:45:36 +0100

    Add project files.

---
+++ HackerRankSolutions/Algo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    class Algo
+    {
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+            int leftToRightSum = 0;
+            int rightToLeftSum = 0;
+            int decrement = 1;
+
+            for(int i = 0; i < arr.Count; i++)
+            {
+                int len = arr[i].Count - decrement;
+                leftToRightSum += arr[i][i];
+                rightToLeftSum += arr[i][len];
+
+                decrement++;
+            }
+
+            return Math.Abs(leftToRightSum - rightToLeftSum);
+        }
+    }
+}
---
+++ HackerRankSolutions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int res = Algo.DiagonalDifference(new List<List<int>>
+            { 
+                new List<int>{ 3, 4, 7, 5 },
+                new List<int>{ 4, 6, 4 , 7},
+                new List<int>{ 2, 4, 6, 6 },
+                new List<int>{ 2, 4, 6, 6 }
+            });
+
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
